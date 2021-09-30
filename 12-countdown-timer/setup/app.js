81eeae2eda3ecae586a0e3f8e56e8c29429c4856
Emily Butler <emilybutler382@gmail.com>
2021-09-30 18:25:17 -0500

    little js

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -22,3 +22,23 @@ const weekdays = [
   "Saturday",
 ];
 
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2022, 5, 24, 11, 30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
